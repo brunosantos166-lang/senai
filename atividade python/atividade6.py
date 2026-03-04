4f57281d39db11c49c4aadc78731a3bd46524688
brunosantos166-lang <bruno_santos166@estudante.sesisenai.org.br>
2026-03-04 20:17:19 +0000

    update

---
+++ atividade python/atividade6.py
@@ -0,0 +1,8 @@
+nome =input("insira o nome:")
+idade = int (input("insira a idade: "))
+
+#repete quando a idade for valida
+while idade > 120 or idade < 0:
+    print = int(input("idade(anos comletos - ate 120)"))
+dias_vida = idade * 365
+print (f" {nome},voce viveu {dias_vida}")
---
+++ atividade python/atividade8.py
@@ -0,0 +1,5 @@
+peso = float(imput("insira o seu peso"))
+peso_engordor = peso * 1.15
+peso_emagrecer = peso * 0.80
+print("o peso se a pessoa engordar 15%",peso_engordor) 
+print ("o peso se a pessoa emagrecer 20%",peso_emagrecer)
